eae251420ad43ed892b273093fe704fc67f39674
Bennie Mosher <benniemosher@gmail.com>
2015-12-29 12:02:20 -0700

    Setting google-analytics.com script call to always be HTTPS

--- src/ga-lite.js
+++ src/ga-lite.js
@@ -5,7 +5,7 @@
         window.galite = window.galite || {};
         var req = new XMLHttpRequest();
         var urlBase = (
-            '//www.google-analytics.com/collect?' +
+            'https://www.google-analytics.com/collect?' +
             'cid=' + (localStorage.uid = localStorage.uid || Math.random() + '.' + Math.random()) +
             '&v=1' +
             '&tid=' + galite.UA +
