938728216fd39f6043d97f6be895324a29903dbe
Jesse Luoto <jesse.luoto@gmail.com>
2017-07-29 23:28:01 +0300

    Add babel transforms

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,29 @@ const webpack = require('webpack')
 
 module.exports = {
   entry: {
-    "ga-lite": "./src/ga-lite",
-    "ga-lite.min": "./src/ga-lite"
+    'ga-lite': './src/ga-lite',
+    'ga-lite.min': './src/ga-lite'
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-    libraryTarget: "amd"
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'amd'
   },
-  target: "web",
+  target: 'web',
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       include: /\.min\.js$/
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 }
