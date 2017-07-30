8cde2fa4c3e7f6b9ee71691032e3b33b7a2a86cf
Jesse Luoto <jesse.luoto@gmail.com>
2017-07-30 21:33:41 +0300

    Fix running Webpack before wdio tests

--- test/browser-tests/wdio.conf.js
+++ test/browser-tests/wdio.conf.js
@@ -107,6 +107,7 @@ exports.config = {
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
   services: ['phantomjs', 'webpack'],
+  webpackConfig: require('../../webpack.config.js'),
   //
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
