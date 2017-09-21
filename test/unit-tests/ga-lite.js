6ff80b923dfe8883a1a3ef4373e19e6ef21e9ee6
Jesse Luoto <jesse.luoto@gmail.com>
2017-09-21 19:42:58 +0300

    Add named tracker command API

--- test/unit-tests/ga-lite.js
+++ test/unit-tests/ga-lite.js
@@ -1,16 +1,20 @@
 import galite from '../../src/ga-lite'
+import Tracker, { DEFAULT_TRACKER_NAME } from '../../src/tracker'
+import MockStorage from './mock-storage'
 import { expect } from 'chai'
 import { clearStore, getAllTrackers } from '../../src/tracker-store'
 
 describe('galite', () => {
   beforeEach(() => {
-    global.window = {}
+    global.window = { screen: {} }
     global.navigator = {}
+    global.document = { location: {} }
   })
 
   afterEach(() => {
     delete global.window
     delete global.navigator
+    delete global.document
     clearStore()
   })
 
@@ -29,5 +33,65 @@ describe('galite', () => {
   it(`should create a new tracker on "create" command`, () => {
     galite('create', 'UA-XXXXXX')
     expect(getAllTrackers().length).to.eql(1)
+  })
+
+  it('should expose the tracker', () => {
+    const trackerName = 'myTracker'
+    galite('create', 'UA-XXXXXX', 'auto', trackerName)
+    const tracker = galite.getByName(trackerName)
+    expect(tracker).to.be.instanceof(Tracker)
+  })
+
+  it('should call tracker functions', (done) => {
+    const timestamp = Date.now()
+
+    const localStorage = new MockStorage()
+    localStorage.setItem('uid', '12345')
+    global.window.localStorage = localStorage
+
+    galite('create', 'UA-XXXXXX', 'auto')
+    const tracker = galite.getByName(DEFAULT_TRACKER_NAME)
+    tracker._getTime = () => timestamp
+
+    tracker._sendTo = (url) => {
+      expect(url).to.eql(
+        'https://www.google-analytics.com/collect' +
+        '?v=1&ul=en-us&de=UTF-8' +
+        '&cid=12345' +
+        '&tid=UA-XXXXXX' +
+        '&t=pageview' +
+        '&z=' + timestamp
+      )
+      done()
+    }
+
+    galite('send', 'pageview')
+  })
+
+  it('should call named tracker functions', (done) => {
+    const timestamp = Date.now()
+    const trackerName = 'myTracker'
+
+    const localStorage = new MockStorage()
+    localStorage.setItem('uid', '12345')
+    global.window.localStorage = localStorage
+
+    galite('create', 'UA-XXXXXX', 'auto', trackerName)
+    const tracker = galite.getByName(trackerName)
+    tracker._getTime = () => timestamp
+
+    tracker._sendTo = (url) => {
+      expect(url).to.eql(
+        'https://www.google-analytics.com/collect' +
+        '?v=1&ul=en-us&de=UTF-8' +
+        '&cid=12345' +
+        '&tid=UA-XXXXXX' +
+        '&t=pageview' +
+        '&z=' + timestamp
+      )
+      done()
+    }
+
+    galite(`${trackerName}.send`, 'pageview')
   })
 })
