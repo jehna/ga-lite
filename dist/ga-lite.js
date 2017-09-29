4614b8721e97c3f1df08c811186d6a8c609d7258
Jesse Luoto <jesse.luoto@gmail.com>
2017-09-29 23:47:41 +0300

    Build version 2.0.1

--- dist/ga-lite.js
+++ dist/ga-lite.js
@@ -351,19 +351,22 @@ function create(trackingId, cookieDomain) {
 "use strict";
 /* harmony export (immutable) */ __webpack_exports__["a"] = sendTo;
 function sendTo(url) {
-  if (navigator.sendBeacon) {
-    navigator.sendBeacon(url);
-  } else {
-    try {
-      var req = new window.XMLHttpRequest();
-      req.open('GET', url, false);
-      req.send();
-    } catch (e) {
-      // IE9 throws an error with cross-site XMLHttpRequest so
-      // we fall back to simple image request
-      var i = new window.Image();
-      i.src = url;
+  if (typeof navigator !== 'undefined' && navigator.sendBeacon) {
+    var didSucceed = navigator.sendBeacon(url);
+    if (didSucceed) {
+      return;
     }
+  }
+
+  try {
+    var req = new window.XMLHttpRequest();
+    req.open('GET', url, false);
+    req.send();
+  } catch (e) {
+    // IE9 throws an error with cross-site XMLHttpRequest so
+    // we fall back to simple image request
+    var i = new window.Image();
+    i.src = url;
   }
 }
 
