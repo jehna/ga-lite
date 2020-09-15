037a43e93262e5794e39871e7d5919cbf7af1587
Jesse Luoto <jesse.luoto@gmail.com>
2020-09-15 21:38:50 +0300

    Fix lint errors

--- src/do-not-track-enabled.js
+++ src/do-not-track-enabled.js
@@ -2,7 +2,7 @@ export default function doNotTrackEnabled() {
   if (typeof navigator === 'undefined' || typeof window === 'undefined') {
     return false
   }
-  
+
   const dntNumber = parseInt(
     navigator.msDoNotTrack || // Internet Explorer 9 and 10 vendor prefix
     window.doNotTrack || // IE 11 uses window.doNotTrack
