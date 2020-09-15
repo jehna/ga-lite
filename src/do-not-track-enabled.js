dc1ac3f71ef5981c485586d440a9e09235d84116
Kid <44045911+kidonng@users.noreply.github.com>
2020-09-16 02:43:53 +0800

    Improve SSR compatibility (#86)
    * Improve SSR compatibility
    
    * Fix lint errors
    
    Co-authored-by: Jesse Luoto <jesse.luoto@gmail.com>

--- src/do-not-track-enabled.js
+++ src/do-not-track-enabled.js
@@ -1,4 +1,8 @@
 export default function doNotTrackEnabled() {
+  if (typeof navigator === 'undefined' || typeof window === 'undefined') {
+    return false
+  }
+
   const dntNumber = parseInt(
     navigator.msDoNotTrack || // Internet Explorer 9 and 10 vendor prefix
     window.doNotTrack || // IE 11 uses window.doNotTrack
