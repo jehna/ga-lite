aac01c8fbfb14c13c16bca292c53bd73975f6991
Jesse Luoto <jesse.luoto@gmail.com>
2020-09-18 21:45:33 +0300

    Upgrade most dependencies (#126)

--- cypress/integration/galite.spec.js
+++ cypress/integration/galite.spec.js
@@ -8,7 +8,7 @@ context('ga-lite', () => {
     let sr = ''
     cy.server()
     cy.route(/www.google-analytics.com\/collect/, '').as('gaCollect')
-    cy.window().then(win => {
+    cy.window().then((win) => {
       cy.stub(win.navigator, 'sendBeacon')
       win.localStorage.setItem('uid', '11223344')
       sr = `${win.screen.availWidth}x${win.screen.availHeight}`
@@ -18,7 +18,7 @@ context('ga-lite', () => {
     cy.window().invoke('galite', 'send', 'pageview')
     cy.wait('@gaCollect')
 
-    cy.get('@gaCollect').should(xhr => {
+    cy.get('@gaCollect').should((xhr) => {
       const url = new URL(xhr.url)
 
       expect(url.search).to.include('v=1&ul=en-us&de=UTF-8')
@@ -26,7 +26,7 @@ context('ga-lite', () => {
         /&dl=http:\/\/localhost:\d+\/cypress\/fixtures\/index.html/
       )
       expect(url.search).to.include('&dt=')
-      expect(url.search).to.include('&sd=24-bit')
+      expect(url.search).to.match(/&sd=\d\d-bit/)
       expect(url.search).to.include('&sr=' + sr)
       expect(url.search).to.include('vp=1280x720')
       expect(url.search).to.include('&dr=')
@@ -40,9 +40,9 @@ context('ga-lite', () => {
   it('should not call endpoint if tracker has been disabled', () => {
     cy.server()
     cy.route(/www.google-analytics.com\/collect/, '').as('gaCollect')
-    cy.window().then(win => {
+    cy.window().then((win) => {
       cy.stub(win.navigator, 'sendBeacon')
-      win["ga-disable-UA-666"] = true
+      win['ga-disable-UA-666'] = true
     })
 
     cy.window().invoke('galite', 'create', 'UA-666', 'auto', 'badTracker')
@@ -50,7 +50,7 @@ context('ga-lite', () => {
     cy.window().invoke('galite', 'goodTracker.send', 'pageview')
     cy.window().invoke('galite', 'badTracker.send', 'pageview')
 
-    cy.get('@gaCollect').should(xhr => {
+    cy.get('@gaCollect').should((xhr) => {
       const url = new URL(xhr.url)
       expect(url.search).to.include('&tid=UA-777')
       expect(url.search).not.to.include('&tid=UA-666')
